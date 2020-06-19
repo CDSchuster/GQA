311a65ee1b31e9bf206fb2d5a0230279f9d7a47f
claudio <claudio.schuster.93@gmail.com>
2020-06-19 17:42:29 -0300

    Modifique el esqueleto del main para que se haga par alineamientos de a pares.

--- main.py
+++ main.py
@@ -2,42 +2,63 @@ from Bio import SeqIO
 import pandas as pd
 import argparse
 
-def run_mafft_add(ref_seq, seqs):
-    """Runs mafft, adding the seqs to the reference sequence"""
-    #Aca habria que hacer que el MSA tenga un nombre estandar para que el usuario sepa lo que es
-    return MSA_filename
+def pairwise_align(ref_seq, seq):
+    """Runs pairwise alignment for one sequence and the reference"""
+    #COMPLETAR
+    #Puede ser manual, con MAFFT o Blast. Si se hace con programa debe
+    #devolver el nombre del archivo donde esta guardado el alineamiento
+    return alignment_filename
 
-def MSA_parse(ref_seq, seqs):
-    """Runs function run_mafft_add, parses the MSA file and returns it as a
-    list of SeqIO objects"""
-    MSA_filename=run_mafft_add(ref_seq, seqs)
-    return list(SeqIO.parse(MSA_filename, "fasta"))
+def alignment_parser(ref_seq, seq):
+    """Return the parsed pairwise alignment"""
+    alignment_filename=pairwise_align(ref_seq, seq)
+    #Esta funcion tiene sentido si el alineamiento se hace con Blast/MAFFT
+    #COMPLETAR
+    return parsed_alignment
+
 
 def parse_ORF_positions(table_filename):
-    table=pd.read_csv(table_filename)
-    #si hay una insercion en la referencia va a haber desfasaje
-    #por lo que habria que ajustar las posiciones de ORFS en genoma sin alinear
-    #en funcion de la cantidad de gaps detectados
-    #esta funcion deberia hacer eso, devolver una lista o diccionario con las
-    #posiciones ajustadas al MSA
-    return ORF_pos
+    """Parses a table file with the ORF
+    positions and returns them as a dataframe"""
+    #Dependiendo el formato de la tabla es posible que haga falta especificar
+    #parametros en el read_csv
+    #COMPLETAR
+    return pd.read_csv(table_filename)
+
+def adjust_ORF_pos(ORF_pos, alignment):
+    """Adjusts ORF positions according to gap
+    insertions in the reference sequence"""
+    #Debe hacer un conteo de gaps en la referencia recorriendo el alineamiento
+    #pareado y reajustar las posiciones de inicio y fin de los ORFs.
+    #Debe devolver ls posiciones ajustadas en formato lista o diccionario
+    #COMPLETAR
+    return adjusted_ORFs
 
 def analyze_seq_ORF(seq, ORF):
     """For a specific ORF in a sequence counts nucleotides,
     gaps and missing data, computes percentajes and returns
     them in a tuple with 3 values"""
+    #COMPLETAR
     return stats_tuple
 
-def analyze_MSA(ref_seq, seqs, table_filename):
-    """Executes MSA_parse, parse_ORF_positions and generates an array with all
-    results of analysis for every ORF in every sequence"""
-    MSA=MSA_parse(ref_seq, seqs)
+def analyze_seqs(ref_seq, input_seqs, table_filename):
+    """Iterates every sequence to analyze, performs the
+    analysis and returns the results for all the sequences"""
     ORF_pos=parse_ORF_positions(table_filename)
-    return [[analyze_seq_ORF(seq, ORF) for ORF in ORF_pos] for seq in MSA]
+    seqs=list(SeqIO.parse(input_seqs, "fasta"))
+    results=[]
+    for seq in seqs:
+        seq_result=[]
+        alignment=alignment_parser(ref_seq, seq)
+        adjusted_ORFs=adjust_ORF_pos(ORF_pos, alignment)
+        for ORF in adjusted_ORFs:
+            seq_result.append(analyze_seq_ORF(seq, ORF))
+    return results
 
-def write_analysis_results(ref_seq, seqs, table_filename, output_filename):
-    """writes MSA analysis results to a csv file"""
-    MSA_analysis=analyze_MSA(ref_seq, seqs, table_filename)
+def write_analysis_results(ref_seq, input_seqs, table_filename, output_filename):
+    """writes sequence analysis results to a csv file"""
+    results=analyze_seqs(ref_seq, input_seqs, table_filename)
+    #COMPLETAR
     #Tiene que devolver un csv con los resultados, formato de la tabla a discutir
     return 0
 
@@ -45,10 +66,10 @@ def parse_arguments():
     """parses all necessary arguments"""
     parser = argparse.ArgumentParser()
     parser.add_argument("-ref", dest="ref_seq", nargs='+',
-                        help="The sequence with which other sequences will be compared")
+                        help="The reference sequence with which other sequences will be compared")
     parser.add_argument("-seqs", dest="seqs", nargs='+',
                         help="Sequences to be analyzed")
-    parser.add_argument("-t", dest="table", nargs="+", type=int,
+    parser.add_argument("-t", dest="table", nargs="+",
                         help="Table with ORF annotations")
     parser.add_argument("-o", dest="output_file", nargs='+',
                         help="Name of the output file")
